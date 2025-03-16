de683b602f404e7ce025fb65399cd8b1e753ff37
zwev <a.ochoa240@gmail.com>
2025-03-16 14:51:03 -0700

    changed message on lose

--- badjs/numberGuess/js/script.js
+++ badjs/numberGuess/js/script.js
@@ -60,7 +60,7 @@ function checkGuess(){
         document.querySelector("#attempts").textContent = 7 - attempts;
         document.querySelector("#guesses").textContent += guess + ", ";
         if (attempts == 7) {
-            feedback.textContent = "Sorry, you lost!";
+            feedback.textContent = "Sorry, you lost!" +\n "The number was: " + randomNumber;
             feedback.style.color = "red";
             losses +=1 ;
             gameOver();
